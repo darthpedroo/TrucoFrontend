ea40fe409dad964953cb718037e0797b88b7c1e8
darthpedroo <pedrovillarino06@gmail.com>
2024-05-09 23:49:01 -0300

    Modified vue componentes to show cards

---
+++ src/components/Mano.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+import { useCardStore } from '@/stores/cards';
+const store = useCardStore()
+
+</script>
+
+<template>
+
+    <h1>CARTAS: {{store.hand_cards.cards}}</h1>
+</template>
--- src/components/Sala.vue
+++ src/components/Sala.vue
@@ -8,19 +8,29 @@
         {{ user }}
       </li>
     </ul>
+
+    <Mano>
+      
+    </Mano>
+
   </div>
 </template>
 
 
 <script setup>
+
     import { useRoute } from 'vue-router';
     import { ref, reactive } from 'vue';
     import { useUserStore } from '@/stores/user';
+    import Mano from './Mano.vue'
+
+
     const store = useUserStore()
     const route = useRoute();
     const SalaId = ref(route.params.salaId);
 
-  
+    
+
 </script>
 
 
