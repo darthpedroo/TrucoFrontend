a99aea72be416711d4840c782c1556ecfdec96c5
darthpedroo <pedrovillarino06@gmail.com>
2024-06-19 16:18:16 -0300

    a

--- src/api/ApiHandler.js
+++ src/api/ApiHandler.js
@@ -1,15 +1,31 @@
-import api from "./Api"
+import api from "./Api";
+
+function loginUser(username, password) {
+  return api()
+    .post('api/token/', { username, password })
+    .then(response => {
+      console.log("GOT THE TOKEN SUCCESSFULLY: ", response.data);
+      localStorage.setItem('authTokens', JSON.stringify(response.data));
+    })
+    .catch(error => {
+      console.error('An error occurred during obtention of token', error.response?.data || error.message);
+      throw error.response?.data || error;
+    });
+}
 
 export function registerUser(data) {
-    console.log('data: ', data);
-    return api()
-      .post('register', data)
-      .then(response => {
-        console.log('Registration successful', response.data);
-        return response.data;
-      })
-      .catch(error => {
-        console.error('An error occurred during registration', error.response?.data || error.message);
-        throw error.response?.data || error;
-      });
-  }
+  return api()
+    .post('register', data)
+    .then(response => {
+      console.log('Registration successful', response.data);
+      const { email, ...login_data } = response.data;
+
+      console.log("LOGIN_DATA: ", login_data);
+      loginUser(login_data.username, login_data.password); // Pass username and password separately
+      return response.data;
+    })
+    .catch(error => {
+      console.error('An error occurred during registration', error.response?.data || error.message);
+      throw error.response?.data || error;
+    });
+}
